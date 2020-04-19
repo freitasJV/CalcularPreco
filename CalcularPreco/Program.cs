ce121d3e0f5a2037563b1da3b043d3add1359e19
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-04-19 14:38:40 -0300

    Add project files.

---
+++ CalcularPreco/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalcularPreco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cod1, cod2, qtde1, qtde2;
+            double vlr1, vlr2, resultado;
+
+            Console.WriteLine("Digite o código, quantidade e valor unitário na sequência, separado por espaços. Exemplo: 1 15 10.00");
+            string[] entrada = Console.ReadLine().Split(' ');
+            cod1 = int.Parse(entrada[0]);
+            qtde1 = int.Parse(entrada[1]);
+            vlr1 = double.Parse(entrada[2],CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite o código, quantidade e valor unitário na sequência, separado por espaços. Exemplo: 1 15 10.00");
+            entrada = Console.ReadLine().Split(' ');
+            cod2 = int.Parse(entrada[0]);
+            qtde2 = int.Parse(entrada[1]);
+            vlr2 = double.Parse(entrada[2],CultureInfo.InvariantCulture);
+
+            resultado = vlr1 * qtde1 + vlr2 * qtde2;
+
+            Console.WriteLine($"VALOR A PAGAR: R$ {resultado.ToString("F2",CultureInfo.InvariantCulture)}");
+        }
+    }
+}
